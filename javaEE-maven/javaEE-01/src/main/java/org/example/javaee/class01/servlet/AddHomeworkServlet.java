a76e7d0756ceec0b13d446972c3b5b4b62c0c42e
yeyi-a11y <1585216674@qq.com>
2020-03-26 03:10:37 +0800

    改造成maven项目

---
+++ javaEE-maven/javaEE-01/src/main/java/org/example/javaee/class01/servlet/AddHomeworkServlet.java
@@ -0,0 +1,59 @@
+package org.example.javaee.class01.servlet;
+
+import org.example.javaee.class01.jdbc.StudentHomeworkJdbc;
+import org.example.javaee.class01.model.Homework;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+@WebServlet("/addh")
+
+public class AddHomeworkServlet extends HttpServlet {
+
+    @Override
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Homework h = new Homework();
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html; charset=UTF-8");//设置编码，以防表单提交的内容乱码
+        /**
+         * 赋值
+         */
+        h.setTitle(req.getParameter("homework_title"));
+        h.setContent(req.getParameter("homework_content"));
+        h.setCreate_time(new Date());
+      // System.out.println(new Date());
+        //SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = null;
+        try {
+            date = sdf.parse(req.getParameter("update_time").replace("T"," ").concat(":00"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        h.setUpdate_time(date);
+        //System.out.println(req.getParameter("update_time").replace("T"," "));
+        //h.setUpdate_time(req.getParameter("update_time"));
+
+        boolean result = StudentHomeworkJdbc.addHomework(h);
+        if(result) {
+            resp.setHeader("refresh", "2;url=localhost:8080");
+            resp.getWriter().println("发布作业成功");
+
+        }else{
+            resp.setHeader("refresh", "2;url=/jsp/addhomework.jsp");
+            resp.getWriter().println("发布作业失败" );
+
+        }
+    }
+
+}
---
+++ javaEE-maven/javaEE-01/src/main/webapp/jsp/list.jsp
@@ -0,0 +1,79 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 叶宗保1
+  Date: 2020/3/12
+  Time: 18:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page import="org.example.javaee.class01.model.StudentHomework" %>
+
+<%@ page import="java.util.List" %>
+
+<%@ page import="org.example.javaee.class01.jdbc.StudentHomeworkJdbc" %>
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+
+<head>
+
+    <title>学生作业列表</title>
+
+</head>
+
+<body>
+
+<table align="center" width="960" border="1"
+
+       bgcolor="black" cellpadding="1" cellspacing="1">
+
+    <tr align="center" bgcolor="#7fffd4" height="50">
+
+        <td>ID</td>
+
+        <td>学生学号</td>
+
+        <td>作业编号</td>
+
+        <td>作业标题</td>
+
+        <td>作业内容</td>
+
+        <td>提交时间</td>
+
+    </tr>
+
+    <%
+        List<StudentHomework> list = StudentHomeworkJdbc.selectAll();
+        if(null == list || list.size() <= 0){
+            out.print("None data.");
+        }else {
+            for (StudentHomework sh : list){
+    %>
+
+    <tr align="center" bgcolor="white" height="30">
+
+        <td><%=sh.getId()%></td>
+
+        <td><%=sh.getStudent_Id()%></td>
+
+        <td><%=sh.getHomework_Id()%></td>
+
+        <td><%=sh.getHomework_title()%></td>
+
+        <td><%=sh.getHomework_content()%></td>
+
+        <td><%=sh.getCreate_time()%></td>
+
+    </tr>
+
+    <%
+            }
+        }
+    %>
+
+</table>
+
+</body>
+
+</html>
